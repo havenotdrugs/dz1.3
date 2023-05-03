773ec4e57cef9977322a97eece668b4ad3331e74
havenotdrugs <132453806+havenotdrugs@users.noreply.github.com>
2023-05-03 14:18:06 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+//Задание 1.3
+int main() {
+  int a, b;
+  cout << "Привет. Я умножаю заданные тобой числа.\nВведи первое число: ";
+  cin >> a;
+  cout << "Введи второе число: ";
+  cin >> b;
+  cout << "Произведение твоих чисел - " << a * b;
+  return 0;
+}
